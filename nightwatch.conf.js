0a626f437bb3d547b851742f75de777b33858a06
Bakhtiyor <baxtiyorsohibnazarov@gmail.com>
2025-04-06 22:30:50 +0100

    Fixed nightwatch test

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -14,16 +14,16 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['nightwatch','nightwatch/examples'],
+  src_folders: ['test','nightwatch/examples'],
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
   page_objects_path: ['nightwatch/page-objects'],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
-  //custom_commands_path: ['nightwatch/custom-commands'],
+  custom_commands_path: ['nightwatch/custom-commands'],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
-  //custom_assertions_path: ['nightwatch/custom-assertions'],
+  custom_assertions_path: ['nightwatch/custom-assertions'],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   plugins: [],
@@ -40,7 +40,7 @@ module.exports = {
   test_settings: {
     default: {
       disable_error_log: false,
-      launch_url: 'http://127.0.0.1:3000/dashboard',
+      launch_url: 'http://localhost',
 
       screenshots: {
         enabled: false,
@@ -82,63 +82,12 @@ module.exports = {
       }
     },
     
-    //////////////////////////////////////////////////////////////////////////////////
-    // Configuration for using the browserstack.com cloud service                    |
-    //                                                                               |
-    // Please set the username and access key by setting the environment variables:  |
-    // - BROWSERSTACK_USERNAME                                                       |
-    // - BROWSERSTACK_ACCESS_KEY                                                     |
-    // .env files are supported                                                      |
-    //////////////////////////////////////////////////////////////////////////////////
-    browserstack: {
-      selenium: {
-        host: 'hub.browserstack.com',
-        port: 443
-      },
-      // More info on configuring capabilities can be found on:
-      // https://www.browserstack.com/automate/capabilities?tag=selenium-4
-      desiredCapabilities: {
-        'bstack:options': {
-          userName: '${BROWSERSTACK_USERNAME}',
-          accessKey: '${BROWSERSTACK_ACCESS_KEY}'
-        }
-      },
-
-      disable_error_log: true,
-      webdriver: {
-        timeout_options: {
-          timeout: 60000,
-          retry_attempts: 3
-        },
-        keep_alive: true,
-        start_process: false
-      }
-    },
-
-    'browserstack.local': {
-      extends: 'browserstack',
-      desiredCapabilities: {
-        'browserstack.local': true
-      }
-    },
-    
-    'browserstack.chrome': {
-      extends: 'browserstack',
-      desiredCapabilities: {
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-          w3c: true
-        }
-      }
-    },
-    
-    'browserstack.local_chrome': {
-      extends: 'browserstack.local',
-      desiredCapabilities: {
-        browserName: 'chrome'
-      }
-    },
-    
   },
+  
+  usage_analytics: {
+    enabled: true,
+    log_path: './logs/analytics',
+    client_id: '7b14f264-1b55-4e87-a1c6-1a5b73bd73e6'
+  }
   
 };
